12418ecac77ddf2f6a3b2b72be286d66e85c58d6
Priyanka <pxa4576@garagescript.org>
2018-02-17 15:23:01 -0800

    solution 7 to js1

--- curriculum/js1/7.js
+++ curriculum/js1/7.js
@@ -5,8 +5,18 @@
  * @returns {boolean}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (num,i=2) => {
+  if(num===1)
+    return false;
+  if(num===2)
+    return true;
+  if(num%i===0)
+    return false;
+  if(num%i===1)
+    return true;
+  //  if(num%i===!0)
+  // return false;
+  return solution(num,i+1);
 };
 
 module.exports = {
