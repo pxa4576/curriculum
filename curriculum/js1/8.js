6d4e1d2d9816e80782a3fee9c7bd11c8d5139f17
Priyanka <pxa4576@garagescript.org>
2018-03-03 14:25:13 -0800

    solution to problem 8

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -5,8 +5,15 @@
  * @returns {number}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a,b,gcd=1,div=1) => {
+  if(a==0 || b==0)
+    return;
+if(a%div==0 && b%div==0)
+   x=div;
+if(div >=a || div >=b)
+  return gcd = x;
+return solution(a,b,gcd,div+1)
+
 };
 
 module.exports = {
