1d3506d690f4fd49201debca2b0bca52658ac3a9
Priyanka <pxa4576@garagescript.org>
2018-03-03 16:39:54 -0800

    solution to problem 8

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -6,11 +6,8 @@
  */
 
 const solution = (a,b,gcd=1,div=1) => {
-  if (div>a || div>b)
-    return gcd;
-   if (a%div===0 && b%div===0)
-    gcd=div;
-  if (a===0 || b===0) return;
+  if (div>a || div>b) return gcd;
+   if (a%div===0 && b%div===0) gcd=div;
 return solution(a,b,gcd,div+1);
 
 };
