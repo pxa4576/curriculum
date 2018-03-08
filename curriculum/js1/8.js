c511dbcd1f3b240b5b7dbb2e4979b75e03e6a892
Priyanka <pxa4576@garagescript.org>
2018-03-08 14:46:36 -0800

    solution to problem 8 in js1
    Added spacing before and after equal signs

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -5,10 +5,10 @@
  * @returns {number}
  */
 
-const solution = (a,b,gcd=1,div=1) => {
+const solution = (a, b, gcd = 1, div = 1) => {
   if (div>a || div>b) return gcd;
-   if (a%div===0 && b%div===0) gcd=div;
-return solution(a,b,gcd,div+1);
+   if (a%div === 0 && b%div === 0) gcd = div;
+return solution(a, b, gcd, div+1);
 
 };
 
