5a3b3cd5c92b8161398b945803df1382a8525383
Priyanka <pxa4576@garagescript.org>
2018-04-30 19:25:13 -0700

    Implementation of Array Reduce Function

--- curriculum/js2/9.js
+++ curriculum/js2/9.js
@@ -2,13 +2,15 @@
  * Replicate Array.prototype.reduce and call it gsReduce
  * @returns {[]}
  */
-
-const solution = () => {
-  Array.prototype.gsReduce = function () {
-    // To get the actual array, use this
-    return [];
-  };
-};
+const solution = () => {$
+  Array.prototype.gsReduce  = function(func, accumulator = this[0], index = 0) {$
+   if(this.length === index) return accumulator;$
+    accumulator = func(accumulator,this[index]);
+   return this.gsReduce(func, accumulator, index + 1);$
+  };$
+  const func = (r,i) => {
+    return r+i;
+};$
 
 module.exports = {
   solution,
